c4bdefbfddf4eab478327400263de766583cbf72
Muhamamd Umair <umair@gmail.com>
2025-07-29 11:53:05 +0500

    Dynamic PDF changes in App Serivce Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,20 +11,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        if (App::environment('production')) {
-            $this->app->bind('path.public', function () {
-                return base_path('../../public_html/public');
-            });
-        }
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production')) {
+            $this->app->bind('path.public', function () {
+                return base_path('../../public_html/public');
+            });
+        }
     }
 }
